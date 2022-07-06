1984294a989ee8ae20478399bc618a105419173e
JereUR <jereur.jdv@gmail.com>
2022-07-06 19:01:49 -0300

    More Components and styles

--- app/App.js
+++ app/App.js
@@ -1,12 +1,25 @@
+import { Header } from "./components/Header.js";
 import { Loader } from "./components/Loader.js";
-import { Title } from "./components/Title.js";
+import { PostCard } from "./components/PostCard.js";
+import { Posts } from "./components/Posts.js";
 import { ajax } from "./helpers/ajax.js";
 import api from "./helpers/wp_api.js";
 
 export function App() {
   const d = document,
     $root = d.getElementById("root");
-  $root.appendChild(Title());
+  $root.appendChild(Header());
+  $root.appendChild(Posts());
   $root.appendChild(Loader());
 
+  ajax({
+    url: api.POSTS,
+    cbSuccess: (posts) => {
+      //console.log(posts);
+      let html = "";
+      posts.forEach((post) => (html += PostCard(post)));
+      d.querySelector(".loader").style.display = "none";
+      d.getElementById("posts").innerHTML = html;
+    },
+  });
 }
